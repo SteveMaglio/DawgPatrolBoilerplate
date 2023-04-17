bca697f0d0a363323ba4b25e56f5e522c02152bf
Alison Picerno <alisonpicerno@gmail.com>
2023-04-17 13:32:48 -0400

    new student routes

--- flask-app/src/student/student.py
+++ flask-app/src/student/student.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from src import db, perform_sql_query
 
@@ -17,3 +17,40 @@ def get_students():
 def get_student_by_nuid(NUID):
     query = 'select * from Student where Student.NUID = {0}'.format(NUID)
     return perform_sql_query(query)
+
+@student.route('/students/<NUID>/lock', methods =['GET'])
+def get_student_lock(NUID):
+    query = 'select lock_used from Student where Student.NUID = {0}'.format(NUID)
+    return perform_sql_query(query)
+
+@student.route('/students/<NUID>/health', methods =['GET'])
+def get_student_health_info(NUID):
+    query = 'select height_in_cm, weight_in_lbs from Student where Student.NUID = {0}'.format(NUID)
+    return perform_sql_query(query)
+
+
+@student.route('/product', methods = ['PUT'])
+def update_student_health(NUID):
+
+    # collect data
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    # extract the variables
+    weight = the_data['prod_description']
+    
+    # construct query
+    query = ' UPDATE Student SET weight_in_lbs = " ' 
+    query += weight '", '
+    query += 'WHERE NUid = '", '
+    query += NUID + ')'
+
+    current_app.logger.info(query)
+
+    # execute and committing the insert statement
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return "Success!   "
+
