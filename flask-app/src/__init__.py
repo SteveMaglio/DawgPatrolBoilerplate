909e77c1ebe663361c41c2548bae9daea6c3ce8e
stephenmaglio <magliocchino.s@northeastern.edu>
2023-04-17 11:47:34 -0400

    removed northwind junk

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -59,15 +59,11 @@ def create_app():
 
     # Import the various routes
     from src.views import views
-    from src.customers.customers import customers
-    from src.products.products  import products
     from src.sections.sections  import sections
     from src.machines.machines  import machines
 
     # Register the routes that we just imported so they can be properly handled
     app.register_blueprint(views,       url_prefix='/v')
-    app.register_blueprint(customers,   url_prefix='/c')
-    app.register_blueprint(products,    url_prefix='/p')
     app.register_blueprint(sections,    url_prefix='/s')
     app.register_blueprint(machines,    url_prefix='/s')
 
