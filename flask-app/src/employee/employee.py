8f0804731630fb2f4e0c11119771e20bb55fc591
Alison Picerno <alisonpicerno@gmail.com>
2023-04-19 11:06:51 -0400

    delete employee

--- flask-app/src/employee/employee.py
+++ flask-app/src/employee/employee.py
@@ -112,3 +112,22 @@ def get_products():
     query = 'SELECT machine_id, machine_name, section FROM Machine'
     return perform_sql_query(query)
 
+#fire employee
+@employee.route('/fire_employee', methods = ['DELETE'])
+def clock_out(employee_id):
+    # collect data
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    # extract the variables
+    id = the_data['emp_id']
+
+    query = f'DELETE from Employee WHERE employee_id = {id}'
+    current_app.logger.info(query)
+
+    # execute and committing the insert statement
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return "Success!   "
